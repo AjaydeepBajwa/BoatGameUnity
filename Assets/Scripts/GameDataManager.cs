0a15c356017dc5ae43a553d25a3e9cce9c5687a4
AJAY-MAC <letv4545@gmail.com>
2019-12-04 18:01:29 -0500

    imported more assets(circle, obstacle)

--- Assets/Scripts/GameDataManager.cs
+++ Assets/Scripts/GameDataManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameDataManager : MonoBehaviour
 {
+    private static GameDataManager managerInstance;
+
+
     // Start is called before the first frame update
     void Start()
     {
