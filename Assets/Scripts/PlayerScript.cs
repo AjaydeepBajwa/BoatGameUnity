30e57ad2a1bebc55e872772ceea3ce10e7f3c5c0
AJAY-MAC <letv4545@gmail.com>
2019-12-04 17:05:38 -0500

    Moving left boat on tapping left, right boat on tapping right

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -48,16 +48,45 @@ public class PlayerScript : MonoBehaviour
                     }
                 }
             }
+            float moveAmount = speed * Time.deltaTime;
+            transform.Translate(moveAmount * direction);
 
+            //x coordinate for left boat should be -2.75 to -0.9
+            Vector2 currentPosition = transform.position;
+            //Using Clamp function to keep the boat between two lanes i.e. between minimumX to maximumX
+            currentPosition.x = Mathf.Clamp(currentPosition.x, minimumX, maximumX);
+            transform.position = currentPosition;
         }
+        else
+        {
+            if (Input.mousePosition.x >= Screen.width / 2)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("clicked");
+                    if (direction == Vector2.zero)
+                    {
+                        direction = Vector2.right;
+                    }
+                    else if (direction == Vector2.right)
+                    {
+                        direction = Vector2.left;
+                    }
+                    else if (direction == Vector2.left)
+                    {
+                        direction = Vector2.right;
+                    }
+                }
+            }
+            float moveAmount = speed * Time.deltaTime;
+            transform.Translate(moveAmount * direction);
 
-        float moveAmount = speed * Time.deltaTime;
-        transform.Translate(moveAmount * direction);
-
-        //x coordinate for left boat should be -2.75 to -0.9
-        Vector2 currentPosition = transform.position;
-        //Using Clamp function to keep the boat between two lanes i.e. between minimumX to maximumX
-        currentPosition.x = Mathf.Clamp(currentPosition.x, minimumX, maximumX);
-        transform.position = currentPosition;
+            //x coordinate for left boat should be -2.75 to -0.9
+            Vector2 currentPosition = transform.position;
+            //Using Clamp function to keep the boat between two lanes i.e. between minimumX to maximumX
+            currentPosition.x = Mathf.Clamp(currentPosition.x, minimumX, maximumX);
+            transform.position = currentPosition;
+        }
+       
     }
 }
