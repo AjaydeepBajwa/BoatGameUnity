0c6cf8aaba325ef836f98051112decc99956fb0b
AJAY-MAC <letv4545@gmail.com>
2019-12-05 09:36:50 -0500

    Added Objects and Moving them

---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public GameObject circle;
+    public GameObject obstacle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(circle, transform.position, transform.rotation);
+        Instantiate(obstacle, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
