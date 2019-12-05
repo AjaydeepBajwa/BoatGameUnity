12c1e7510d7d58d853f2f4e1c3c95a2d0e24b1cc
AJAY-MAC <letv4545@gmail.com>
2019-12-05 10:43:13 -0500

    spawning object at random position

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -6,11 +6,21 @@ public class SpawnObjects : MonoBehaviour
 {
     public GameObject circle;
     public GameObject obstacle;
+    public float minPos;
+    public float maxPos;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(circle, transform.position, transform.rotation);
-        Instantiate(obstacle, transform.position, transform.rotation);
+        //Random random = new Random();
+        float[] XPosArray = new float[] { -2.75f, -1, 1, 2.75f};
+        //int randomIndex = random.(0,XPosArray.Length - 1);
+        //int randomPrice = priceArray[randomIndex];
+        //Vector2 objectPosition = new Vector2(Random.;
+        float randomPos = (XPosArray[Random.Range(0, XPosArray.Length)]);
+        Vector2 objectPosition = new Vector2(randomPos, transform.position.y);
+
+        Instantiate(circle,objectPosition, transform.rotation);
+        Instantiate(obstacle, objectPosition, transform.rotation);
     }
 
     // Update is called once per frame
