a63eb94a0322ffa9f827b8a89380a9cef40111eb
AJAY-MAC <letv4545@gmail.com>
2019-12-06 20:20:05 -0500

    Testing,Debugging Blow feature on phone

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -17,21 +17,10 @@ public class PlayerScript : MonoBehaviour
 
     public UIManager uiManager;
 
-    //Blowing Variables
-    AudioClip microphoneInput;
-    bool microphoneInitialized;
-    public float sensitivity = 100;
-    public bool flapped;
 
     private void Awake()
     {
-        //init microphone input
-        if (Microphone.devices.Length > 0)
-        {
-            microphoneInput = Microphone.Start(Microphone.devices[0], true, 999, 44100);
-            microphoneInitialized = true;
-            Debug.Log("Mic Initialized");
-        }
+        
     }
 
     // Start is called before the first frame update
@@ -51,42 +40,6 @@ public class PlayerScript : MonoBehaviour
         }
         //direction = Vector2.zero;
     }
-
-    public void blowCheck()
-    {
-        //get mic volume
-        int dec = 128;
-        float[] waveData = new float[dec];
-        int micPosition = Microphone.GetPosition(null) - (dec + 1); // null means the first microphone
-        microphoneInput.GetData(waveData, micPosition);
-
-        // Getting a peak on the last 128 samples
-        float levelMax = 0;
-        for (int i = 0; i < dec; i++)
-        {
-            float wavePeak = waveData[i] * waveData[i];
-            if (levelMax < wavePeak)
-            {
-                levelMax = wavePeak;
-                Debug.Log("Noise Level: " +levelMax);
-            }
-        }
-        float level = Mathf.Sqrt(Mathf.Sqrt(levelMax));
-        
-
-        if (level > sensitivity && !flapped)
-        {
-            Debug.Log("Blow Accepted");
-            flapped = true;
-            uiManager.buttons[0].gameObject.SetActive(true);
-        }
-        
-        if (level < sensitivity && flapped)
-        {
-            flapped = false;
-        }
-            
-}
 
     // Update is called once per frame
     void Update()
@@ -155,7 +108,7 @@ public class PlayerScript : MonoBehaviour
             currentPosition.x = Mathf.Clamp(currentPosition.x, minimumX, maximumX);
             transform.position = currentPosition;
         }
-        blowCheck();
+       
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
