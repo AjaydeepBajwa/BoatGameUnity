5dcfe34051be88eeaaaa9c7804db380e092df15e
AJAY-MAC <letv4545@gmail.com>
2019-12-09 18:09:20 -0500

    Automatically generated files

---
+++ Assets/Scripts/LeftBoatMoveScript.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class LeftBoatMoveScript : MonoBehaviourPun
+{
+    public int speed;
+    private Vector2 direction;
+    public float minimumX, maximumX;
+    public string boatPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (boatPosition == "left")
+        {
+            //boat is left
+            direction = Vector2.left;
+        }
+        else
+        {
+            //boat is right
+            direction = Vector2.right;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            movePlayer();
+        }
+
+
+    }
+
+    private void movePlayer()
+    {
+        if (boatPosition == "left")
+        {
+            if (Input.mousePosition.x <= Screen.width / 2)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("clicked");
+                    //play boat move sound
+                    //uiManager.playBoatMoveSound();
+                    if (direction == Vector2.zero)
+                    {
+                        direction = Vector2.right;
+                    }
+                    else if (direction == Vector2.right)
+                    {
+                        direction = Vector2.left;
+                    }
+                    else if (direction == Vector2.left)
+                    {
+                        direction = Vector2.right;
+                    }
+                }
+            }
+            float moveAmount = speed * Time.deltaTime;
+            transform.Translate(moveAmount * direction);
+
+            //x coordinate for left boat should be -2.75 to -0.9
+            Vector2 currentPosition = transform.position;
+            //Using Clamp function to keep the boat between two lanes i.e. between minimumX to maximumX
+            currentPosition.x = Mathf.Clamp(currentPosition.x, minimumX, maximumX);
+            transform.position = currentPosition;
+        }
+        else
+        {
+            if (Input.mousePosition.x >= Screen.width / 2)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("clicked");
+                    //playing boat move sound
+                    //uiManager.playBoatMoveSound();
+                    if (direction == Vector2.zero)
+                    {
+                        direction = Vector2.right;
+                    }
+                    else if (direction == Vector2.right)
+                    {
+                        direction = Vector2.left;
+                    }
+                    else if (direction == Vector2.left)
+                    {
+                        direction = Vector2.right;
+                    }
+                }
+            }
+            float moveAmount = speed * Time.deltaTime;
+            transform.Translate(moveAmount * direction);
+
+            //x coordinate for left boat should be -2.75 to -0.9
+            Vector2 currentPosition = transform.position;
+            //Using Clamp function to keep the boat between two lanes i.e. between minimumX to maximumX
+            currentPosition.x = Mathf.Clamp(currentPosition.x, minimumX, maximumX);
+            transform.position = currentPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/MultiPlayer Scripts/PlayerSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject playerPrefab = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 boatPosition = new Vector3(-2.75f, -4.87f, 0);
+        PhotonNetwork.Instantiate(playerPrefab.name, boatPosition, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
