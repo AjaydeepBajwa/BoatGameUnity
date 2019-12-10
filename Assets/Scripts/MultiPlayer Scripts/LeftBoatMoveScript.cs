48133e55840746cef118c5fec965d85887930840
AJAY-MAC <letv4545@gmail.com>
2019-12-09 21:09:17 -0500

    Spawning different boats, Added Name Tag to boats/players

--- Assets/Scripts/MultiPlayer Scripts/LeftBoatMoveScript.cs
+++ Assets/Scripts/MultiPlayer Scripts/LeftBoatMoveScript.cs
@@ -9,20 +9,12 @@ public class LeftBoatMoveScript : MonoBehaviourPun
     public int speed;
     private Vector2 direction;
     public float minimumX, maximumX;
-    public string boatPosition;
+    //public string boatPosition;
     // Start is called before the first frame update
     void Start()
     {
-        if (boatPosition == "left")
-        {
-            //boat is left
             direction = Vector2.left;
-        }
-        else
-        {
-            //boat is right
-            direction = Vector2.right;
-        }
+        
     }
 
     // Update is called once per frame
@@ -38,8 +30,6 @@ public class LeftBoatMoveScript : MonoBehaviourPun
 
     private void movePlayer()
     {
-        if (boatPosition == "left")
-        {
             if (Input.mousePosition.x <= Screen.width / 2)
             {
                 if (Input.GetMouseButtonDown(0))
@@ -69,38 +59,6 @@ public class LeftBoatMoveScript : MonoBehaviourPun
             //Using Clamp function to keep the boat between two lanes i.e. between minimumX to maximumX
             currentPosition.x = Mathf.Clamp(currentPosition.x, minimumX, maximumX);
             transform.position = currentPosition;
-        }
-        else
-        {
-            if (Input.mousePosition.x >= Screen.width / 2)
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Debug.Log("clicked");
-                    //playing boat move sound
-                    //uiManager.playBoatMoveSound();
-                    if (direction == Vector2.zero)
-                    {
-                        direction = Vector2.right;
-                    }
-                    else if (direction == Vector2.right)
-                    {
-                        direction = Vector2.left;
-                    }
-                    else if (direction == Vector2.left)
-                    {
-                        direction = Vector2.right;
-                    }
-                }
-            }
-            float moveAmount = speed * Time.deltaTime;
-            transform.Translate(moveAmount * direction);
-
-            //x coordinate for left boat should be -2.75 to -0.9
-            Vector2 currentPosition = transform.position;
-            //Using Clamp function to keep the boat between two lanes i.e. between minimumX to maximumX
-            currentPosition.x = Mathf.Clamp(currentPosition.x, minimumX, maximumX);
-            transform.position = currentPosition;
-        }
+       
     }
 }
--- Assets/Scripts/MultiPlayer Scripts/PlayerSpawner.cs
+++ Assets/Scripts/MultiPlayer Scripts/PlayerSpawner.cs
@@ -6,11 +6,22 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab = null;
+    [SerializeField] private GameObject player2Prefab = null;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 boatPosition = new Vector3(-2.75f, -4.87f, 0);
-        PhotonNetwork.Instantiate(playerPrefab.name, boatPosition, Quaternion.identity);
+        Vector3 boat1Position = new Vector3(-1.7f, -3.2f, 0);
+        Vector3 boat2Position = new Vector3(1.7f, -3.2f, 0);
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, boat1Position, Quaternion.identity);
+        }
+        else if (!PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate(player2Prefab.name, boat2Position, Quaternion.identity);
+        }
+        
     }
 
     // Update is called once per frame
